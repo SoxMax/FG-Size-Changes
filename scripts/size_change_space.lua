4de31a22dde7f17513289ef3906564cb0d55ec52
SoxMax <irishsoxmax@gmail.com>
2021-12-04 13:02:17 -0500

    add support for reach as well

--- scripts/size_change_space.lua
+++ scripts/size_change_space.lua
@@ -1,10 +1,12 @@
+local resizeOptionName = "RESIZETOKEN"
+
 local function registerOptions()
     -- register option for toggling updates to token space on size change
-    OptionsManager.registerOption2("RESIZETOKEN", false, "option_header_token", "option_label_RESIZETOKEN", "option_entry_cycler",
+    OptionsManager.registerOption2(resizeOptionName, false, "option_header_token", "option_label_RESIZETOKEN", "option_entry_cycler",
             { labels = "option_val_on", values="on", baselabel = "option_val_off", baseval="off", default="off"})
 end
 
-local function updateActorSpace(rActor)
+local function getTotalSize(rActor)
     local sizeChange = 0
     local tSizeEffects, nSizeEffectCount = EffectManager35E.getEffectsBonusByType(rActor, "SIZE", true, {"melee", "ranged"})
     if nSizeEffectCount > 0 then
@@ -12,14 +14,60 @@ local function updateActorSpace(rActor)
             sizeChange = sizeChange + effect.mod
         end
     end
-    local size = ActorManager35E.getSize(rActor) + sizeChange
+    return ActorManager35E.getSize(rActor) + sizeChange
+end
+
+local function updateActorSpace(rActor)
+    local size = getTotalSize(rActor)
     DB.setValue(DB.findNode(rActor.sCTNode), "space", "number", SizeChangeData.sizeSpace[size])
+end
+
+local function hasBonusVsTrip(nodeNPC)
+    local sBABGrp = DB.getValue(nodeNPC, "babgrp", "");
+    local aSplitBABGrp = StringManager.split(sBABGrp, "/", true);
+    if #aSplitBABGrp ~= 3 then
+        aSplitBABGrp = StringManager.split(sBABGrp, ";", true);
+    end
+    return ((aSplitBABGrp[3]:find("vs. trip", 1, true) or aSplitBABGrp[3]:find("can't be tripped", 1, true)) ~= nil)
+end
+
+local function updateActorReach(rActor)
+    local sNodeType, nodeActor = ActorManager.getTypeAndNode(rActor)
+    local totalSize = getTotalSize(rActor)
+    if sNodeType == "pc" then -- Is Player
+        DB.setValue(DB.findNode(rActor.sCTNode), "reach", "number", SizeChangeData.sizeTallReach[totalSize])
+    else -- Is NPC
+        local baseSize = ActorManager35E.getSize(rActor)
+        local nSpace, nReach = CombatManager2.getNPCSpaceReach(nodeActor)
+        if nSpace < nReach then -- Extra tall
+            DB.setValue(DB.findNode(rActor.sCTNode), "reach", "number", SizeChangeData.sizeTallReach[totalSize] * nReach / nSpace)
+        elseif totalSize <= 0 then
+            DB.setValue(DB.findNode(rActor.sCTNode), "reach", "number", SizeChangeData.sizeTallReach[totalSize])
+        elseif baseSize > 0 then -- Large or bigger by default
+            if nSpace == nReach then -- Tall
+                DB.setValue(DB.findNode(rActor.sCTNode), "reach", "number", SizeChangeData.sizeTallReach[totalSize])
+            elseif nSpace > nReach then -- Long
+                DB.setValue(DB.findNode(rActor.sCTNode), "reach", "number", SizeChangeData.sizeLongReach[totalSize])
+            end
+        elseif ActorManager35E.isCreatureType(rActor, "dragon,ooze") then
+            DB.setValue(DB.findNode(rActor.sCTNode), "reach", "number", SizeChangeData.sizeLongReach[totalSize])
+        elseif ActorManager35E.isCreatureType(rActor, "elemental,fey,giant,humanoid,monstrous humanoid,outsider") then
+            DB.setValue(DB.findNode(rActor.sCTNode), "reach", "number", SizeChangeData.sizeTallReach[totalSize])
+        elseif hasBonusVsTrip(nodeActor) then
+            DB.setValue(DB.findNode(rActor.sCTNode), "reach", "number", SizeChangeData.sizeLongReach[totalSize])
+        else
+            DB.setValue(DB.findNode(rActor.sCTNode), "reach", "number", SizeChangeData.sizeTallReach[totalSize])
+        end
+    end
 end
 
 -- This function is called on alignment, size or race change in the Combat Tracker
 local function onCtTypeChanged(nodeType)
-    local rActor = ActorManager.resolveActor(nodeType.getParent())
-    updateActorSpace(rActor)
+    if OptionsManager.isOption(resizeOptionName, "on") then
+        local rActor = ActorManager.resolveActor(nodeType.getParent())
+        updateActorSpace(rActor)
+        updateActorReach(rActor)
+    end
 end
 
 local function effectNodeContainsEffect(nodeEffect, sEffect, rTarget, bTargetedOnly, bIgnoreEffectTargets)
@@ -90,9 +138,10 @@ end
 
 -- This function is call whenever any effect in the Combat Tracker changes
 local function onEffectChanged(nodeEffect, bListchanged)
-    if effectNodeContainsEffect(nodeEffect, "SIZE") then
+    if OptionsManager.isOption(resizeOptionName, "on") and effectNodeContainsEffect(nodeEffect, "SIZE") then
         local rActor = ActorManager.resolveActor(nodeEffect.getChild("..."))
         updateActorSpace(rActor)
+        updateActorReach(rActor)
     end
 end
 
